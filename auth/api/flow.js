897c105cc72a8641d6b9486189b63fad13290c5b
DarkSorrow <darksorrow.slade@gmail.com>
2023-02-23 01:14:12 +0100

    Exemple account

--- auth/api/flow.js
+++ auth/api/flow.js
@@ -8,6 +8,7 @@ const router = new Router();
 // https://github.com/onflow/fcl-js/blob/82243080b498d40909def3ef97fa1750ae6bef3f/packages/config/utils/utils.test.js
 
 //https://github.com/onflow/flow-account-api
+//https://github.com/justinbarry/simple-fcl-grpc-test/blob/main/index.js
 // Move a child from a parent account to a new account
 router.post('/api/flow/child/move', (ctx) => {
   // ctx.flow <- flowClient
